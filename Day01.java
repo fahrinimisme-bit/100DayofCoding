2fd3a5ddddad060f3bb5ade055b835e0c2516caf
fahrinimisme-bit <fahrinimisme@gmail.com>
2026-09-03 09:24:08 +0800

    Update Day01.java

--- Day01.java
+++ Day01.java
@@ -1,6 +1,8 @@
 public class Main { 
   public static void main(String[] args) 
-  { System.out.println("Hello World"); 
-  } } / ini adalah komentar jenis pertama 
+  { System.out.println("Hello World");
+    }
+} 
+ / ini adalah komentar jenis pertama 
   // dan ini adalah jenis komentar kedua
-  /** dan ini adalah jenis komentar yang terakhir
+  /** dan ini adalah jenis komentar ketiga
