b6e45881d9de97c31bc25ffcc73fc2c2e8548a87
fahrinimisme-bit <fahrinimisme@gmail.com>
2026-09-03 09:48:56 +0800

    Update Day01.java
    Ralat lagi

--- Day01.java
+++ Day01.java
@@ -1,8 +1,15 @@
-public class Main { 
-  public static void main(String[] args) 
-  { System.out.println("Hello World");
-    }
-} 
-// jenis komentar pertama (Single-line Comment)
-     /*dan ini adalah jenis komentar kedua (Multi-line Comment)*/
-        /**dan ini adalah jenis komentar kedua (Documentation Comment)*/
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
+}
+
+// Single-line Comment
+
+/*
+   Multi-line Comment
+*/
+
+/**
+  Documentation Comment
+ */
