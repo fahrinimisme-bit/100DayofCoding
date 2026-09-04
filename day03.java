ce35b0ff7db934b8acf45a645376d059b197dee6
fahrinimisme-bit <fahrinimisme@gmail.com>
2026-09-04 20:12:54 +0700

    Create day03.java

---
+++ day03.java
@@ -0,0 +1,21 @@
+public class day03 {
+    public static void main(String[] args) {
+      
+ // fungsi dari \n yaitu membuat garis baru
+  System.out.println("UGM:universitas gunung majene\nfakultas:teknik\nprodi:informatika");
+      
+// fungsi dari \t yaitu untuk membuat spasi yang sejajar
+        System.out.println("nama\t: muhammad fadhil fachry");
+      System.out.println("umur\t: 18 tahun");
+      
+// fungsi dari \' yaitu menampilkan tanda petik tunggal 
+  System.out.printiln("saya dipanggil \'adil\' saat disekolah");
+
+  // fungsi dari \" yaitu untuk menampilkan tanda petik ganda
+  System.out.printiln("tan malaka berkata\"padi tumbuh tak berisik\" dibuku karangannya");
+// fungsi dari \\ yaitu untuk menampilkan oppilihan teks
+        System.out.println("mau dipukul tangan mana\t: kanan\\kiri?");
+   //Fungsi \r yaitu mengembalikan kursor ke awal baris untuk menimpa
+        System.out.println("Status: kerja\rStatus: kuliah");
+    }
+}
