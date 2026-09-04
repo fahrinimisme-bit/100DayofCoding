8ae5809f3160890180873ae41cca5eeed554e13c
fahrinimisme-bit <fahrinimisme@gmail.com>
2026-09-04 22:30:53 +0800

    Update day03.java

--- day03.java
+++ day03.java
@@ -1,21 +1,24 @@
 public class day03 {
     public static void main(String[] args) {
       
- // fungsi dari \n yaitu membuat garis baru
-  System.out.println("UGM:universitas gunung majene\nfakultas:teknik\nprodi:informatika");
+        // Fungsi dari \n yaitu membuat garis baru
+        System.out.println("UGM:universitas gunung majene\nfakultas:teknik\nprodi:informatika");
       
-// fungsi dari \t yaitu untuk membuat spasi yang sejajar
+        // Fungsi dari \t yaitu untuk membuat spasi/tab yang sejajar
         System.out.println("nama\t: muhammad fadhil fachry");
-      System.out.println("umur\t: 18 tahun");
+        System.out.println("umur\t: 18 tahun");
       
-// fungsi dari \' yaitu menampilkan tanda petik tunggal 
-  System.out.printiln("saya dipanggil \'adil\' saat disekolah");
+        // Fungsi dari \' yaitu menampilkan tanda petik tunggal
+        System.out.println("saya dipanggil \'adil\' saat disekolah");
 
-  // fungsi dari \" yaitu untuk menampilkan tanda petik ganda
-  System.out.printiln("tan malaka berkata\"padi tumbuh tak berisik\" dibuku karangannya");
-// fungsi dari \\ yaitu untuk menampilkan oppilihan teks
+        // Fungsi dari \" yaitu untuk menampilkan tanda petik ganda
+        System.out.println("tan malaka berkata \"padi tumbuh tak berisik\" dibuku karangannya");
+
+        // Fungsi dari \\ yaitu untuk menampilkan karakter backslash (\)
         System.out.println("mau dipukul tangan mana\t: kanan\\kiri?");
-   //Fungsi \r yaitu mengembalikan kursor ke awal baris untuk menimpa
+
+        // Fungsi dari \r yaitu mengembalikan kursor ke awal baris untuk menimpa teks
         System.out.println("Status: kerja\rStatus: kuliah");
     }
 }
+
