c475321c7baa30e1c0dafd72c87753b4a40183ef
fahrinimisme-bit <fahrinimisme@gmail.com>
2026-09-04 18:16:27 +0700

    Add Biodata class to display personal information

---
+++ day02.java
@@ -0,0 +1,15 @@
+public class Biodata {
+    public static void main(String[] args) {
+       
+      // ini pake println buat nama
+        System.out.println("Nama: Muh. Fadhil Fachry");
+        
+        // pake print biasa, untuk buat alamatnya
+        System.out.print("Alamat: ");
+        System.out.print("wonomulyo\n");
+        
+        // pake printf buat umur sama no wa (format %d sama %s)
+        System.out.printf("Umur: %d tahun\n", 18);
+        System.out.printf("No WA: %s\n", "085311099660");
+    }
+}
