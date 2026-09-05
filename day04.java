6660471bc0c74f2d85586b6a822ab58e8db7f9f3
fahrinimisme-bit <fahrinimisme@gmail.com>
2026-09-05 14:27:38 +0700

    Create day04.java

---
+++ day04.java
@@ -0,0 +1,22 @@
+public class Day4 {
+    public static void main(String[] args) {
+        
+        // Jenis data byte itu data yang ukurannya paling kecil (8 bit)
+        byte umur = 74;
+        System.out.println("Umur presiden\t: " + umur);
+        
+        // Jenis data short itu data yang agak besar ukurannya (16 bit)
+        short penerimaPerKampus = 1500;
+        System.out.println("Penerima per kampus\t: " + penerimaPerKampus);
+        
+        // Jenis data int ini data yang besar dengan ukuran (32 bit)
+        int suaraPrabowoGibran = 96214691;
+        System.out.println("Suara Prabowo-Gibran\t: " + suaraPrabowoGibran + " suara");
+        
+        // Jenis data long ini data yang lumayan besar dengan ukuran (64 bit)
+        long populasiDunia = 8200000000L;
+        System.out.println("Populasi dunia\t: " + populasiDunia + " orang");
+        
+    }
+}
+}
