017681292b841fad6e8d146bfc6e8899a3a0d989
fahrinimisme-bit <fahrinimisme@gmail.com>
2026-09-06 20:12:10 +0800

    Create Day05.java

---
+++ Day05.java
@@ -0,0 +1,16 @@
+public class Day5 {
+    public static void main(String[] args) {
+
+        // penjelasan dari variabel tipe pecahan
+        double tinggiBadan = 166;
+        float beratBadan = 62f;
+
+        // Menampilkan batas nilai tipe data double
+        System.out.println("Nilai Minimalnya double adalah: " + Double.MIN_VALUE);
+        System.out.println("Nilai Maksimalnya double adalah: " + Double.MAX_VALUE);
+
+        // Menampilkan batas nilai tipe data float
+        System.out.println("Nilai Minimalnya float adalah: " + Float.MIN_VALUE);
+        System.out.println("Nilai Maksimalnya float adalah: " + Float.MAX_VALUE);
+    }
+}
