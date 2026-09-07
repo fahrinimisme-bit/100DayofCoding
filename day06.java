5e0e90428d43324978cf3f8fab9271e5b5ffb5f0
fahrinimisme-bit <fahrinimisme@gmail.com>
2026-09-07 20:31:43 +0800

    Create day06.java

---
+++ day06.java
@@ -0,0 +1,13 @@
+/**
+ * day6
+ */
+public class day6 {
+
+    public static void main(String[] args) {
+        char inisial = 'F';
+        boolean sekarangKemarau = true;
+
+        System.out.println(inisial);
+        System.out.println(sekarangKemarau);
+    }
+}
