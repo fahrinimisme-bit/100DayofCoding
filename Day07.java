8f7271028b42d5e92c869261b120525b18166c0a
fahrinimisme-bit <fahrinimisme@gmail.com>
2026-09-08 20:38:12 +0800

    Create Day07.java

---
+++ Day07.java
@@ -0,0 +1,23 @@
+public class day7
+{
+    public static void main(String[] args) {
+        
+        // String berupa Teks biasa
+        String teks = "Hidup jokowi";
+
+        // String berupa Angka (diperlakukan sebagai teks)
+        String angka = "1903";
+
+        // String berupa Simbol 
+        String simbol = "$";
+
+        // String Kombinasi (Teks, Angka, dan Simbol)
+        String kombinasi = "H!dup 70kow1";         
+      
+        System.out.println("Teks biasa\t: " + teks);
+        System.out.println("Angka\t: " + angka);
+        System.out.println("Simbol\t: " + simbol);
+        System.out.println("Kombinasi\t: " + kombinasi);
+    }
+}
+
