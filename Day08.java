bca5adeaba1b37a1ef9967384624bd8724c50a46
fahrinimisme-bit <fahrinimisme@gmail.com>
2026-09-09 19:55:15 +0800

    Create Day08.java

---
+++ Day08.java
@@ -0,0 +1,15 @@
+public class day08 {
+    public static void main(String[] Args) {
+        
+        // deklarasi dan inisialisasi
+        // Menyimpan judul lagu Iwan Fals rilisan lama
+        String fahriLaguIwanFals = "Sarjana Muda (1981)";
+        System.out.println("Lagu Klasik  : " + fahriLaguIwanFals);
+      
+        // update variabel
+        // Memperbarui isi variabel dengan lagu Iwan Fals rilisan berikutnya
+        fahriLaguIwanFals = "Bento (1989)";
+        System.out.println("Lagu Update  : " + fahriLaguIwanFals);
+        
+    }
+}
