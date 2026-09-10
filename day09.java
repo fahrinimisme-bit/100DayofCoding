f115c091aed5dd19b623265f5a3634a76c0091b2
fahrinimisme-bit <fahrinimisme@gmail.com>
2026-09-10 21:11:12 +0800

    Create day09.java

---
+++ day09.java
@@ -0,0 +1,24 @@
+/**
+ * day9
+ */
+public class day9 {
+
+    public static void main(String[] args) {
+        
+
+
+
+        final int jumlahPlanet = 9;
+        final double IPK = 3.88;
+        final char inisial = 'I';
+
+        System.out.println("JUMLAH PLANET       :" + jumlahPlanet);
+        System.out.println("NILAI IPK           :" + IPK);
+        System.out.println("INISIAL IWAN FALS   :" + inisial);
+
+
+
+
+
+    }
+}
